6f9f66f42bb40e3d1ca59fe919f7f1575a2cc975
jbell1991 <joseph.louis.bell@gmail.com>
2020-07-10 12:08:10 -0400

    first pass ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,24 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    hash_table = {}
 
-    return None
+    for idx, weight in enumerate(weights):
+        hash_table[weight] = idx
+        entry = limit - weight
+        if entry in hash_table:
+            idx_2 = hash_table[entry]
+            if idx > idx_2:
+                return idx, idx_2
+            elif idx < idx_2:
+                return idx_2, idx
+            elif idx == idx_2:
+                return idx + 1, idx_2
+    
+    return
+
+weights = [4, 6, 10, 15, 16]
+length = 5
+limit = 21
+
+print(get_indices_of_item_weights(weights, length, limit))
