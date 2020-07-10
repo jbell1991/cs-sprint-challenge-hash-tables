fe439db7ab6a347194e8cdbb6b99d375530e5595
jbell1991 <joseph.louis.bell@gmail.com>
2020-07-10 13:18:52 -0400

    finished ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,25 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    hash_table = {}
+    matches = []
 
-    return result
+    for num in a:
+        hash_table[num] = num
 
+    for key in hash_table.keys():
+        if key and -key in hash_table.keys():
+            if key > 0:
+                matches.append(key)
+    return matches
+    
+    # speed test without using a hashtable
+    # matches = []
+    # for num in a:
+    #     if num and -num in a:
+    #         if num > 0:
+    #             matches.append(num)
+    # return matches 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
