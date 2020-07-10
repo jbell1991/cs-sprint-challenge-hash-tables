32f55da740a1f19dc574c2bb2d224972f9c00fa9
jbell1991 <joseph.louis.bell@gmail.com>
2020-07-10 12:55:06 -0400

    finished ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,16 +2,32 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    hash_table = {}
+    intersections = []
 
-    return result
+    for arr in arrays:
+        for num in arr:
+            if num not in hash_table:
+                hash_table[num] = 1
+            else:
+                hash_table[num] += 1
+                
+    max_value = max(hash_table.values())
+    for key, value in hash_table.items():
+        if value == max_value:
+            intersections.append(key)
+    return intersections
 
 
 if __name__ == "__main__":
-    arrays = []
+    arrays = [[1, 2, 3, 4, 5],
+              [12, 7, 2, 9, 1],
+              [99, 2, 7, 1]]
+    # arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
+    
     print(intersection(arrays))
